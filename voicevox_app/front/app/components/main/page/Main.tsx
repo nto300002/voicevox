53333ddbf9e8ec5427e20d2c08bb3c6d5a3e0b49
nto300002 <samonkntd@gmail.com>
2023-09-11 23:44:13 +0900

    add mainpage

---
+++ voicevox_app/front/app/components/main/page/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Character from '../../layouts/Character'
+
+const Main = () => {
+  return (
+    <div>
+      Main
+      <Character />
+    </div>
+  )
+}
+
+export default Main
